a6ee8c47ecef33071b78599866eeae0b5619bd22
github-action-benchmark <github@users.noreply.github.com>
2025-03-21 17:26:26 +0000

    add Benchmark (googlecpp) benchmark result for 3e545471b7e44a4f1858c6729184d6d4cccd6f50

---
+++ docs/static/ot/data.js
@@ -0,0 +1,50 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1742577986703,
+  "repoUrl": "https://github.com/coinbase/cb-mpc",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "Arash-Afshar@users.noreply.github.com",
+            "name": "Arash Afshar",
+            "username": "Arash-Afshar"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3e545471b7e44a4f1858c6729184d6d4cccd6f50",
+          "message": "feat: first release (#3)",
+          "timestamp": "2025-03-21T10:42:58-06:00",
+          "tree_id": "a14267dcc1c4572d5df58c559399a4a0ec47d107",
+          "url": "https://github.com/coinbase/cb-mpc/commit/3e545471b7e44a4f1858c6729184d6d4cccd6f50"
+        },
+        "date": 1742577986140,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "MPC/OT/BaseOT/Step1_R2S/256",
+            "value": 24440.720551724397,
+            "unit": "us/iter",
+            "extra": "iterations: 29\ncpu: 24439.121482758626 us\nthreads: 1"
+          },
+          {
+            "name": "MPC/OT/BaseOT/Step2_S2R/256",
+            "value": 99063.5975713953,
+            "unit": "us/iter",
+            "extra": "iterations: 7\ncpu: 99062.52885714288 us\nthreads: 1"
+          },
+          {
+            "name": "MPC/OT/BaseOT/OutputR/256",
+            "value": 13775.621254897524,
+            "unit": "us/iter",
+            "extra": "iterations: 51\ncpu: 13774.46854901961 us\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
