01fb73b5525e59ed472345386bbf5dc5f02bdb4d
github-action-benchmark <github@users.noreply.github.com>
2025-03-26 14:10:42 +0000

    add Benchmark (googlecpp) benchmark result for dc7ce2156cfc61366a0f5f70761dacf22a35008b

--- docs/static/core-ec/data.js
+++ docs/static/core-ec/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1742577980235,
+  "lastUpdate": 1742998242551,
   "repoUrl": "https://github.com/coinbase/cb-mpc",
   "entries": {
     "Benchmark": [
@@ -72,6 +72,78 @@ window.BENCHMARK_DATA = {
             "value": 57.21650626277122,
             "unit": "us/iter",
             "extra": "iterations: 12215\ncpu: 57.214875317232924 us\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "Arash-Afshar@users.noreply.github.com",
+            "name": "Arash Afshar",
+            "username": "Arash-Afshar"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "dc7ce2156cfc61366a0f5f70761dacf22a35008b",
+          "message": "chore: cleanup (#5)\n\n* fix: properly publish commit in benchmark page\n* feat: support go 1.24\n* chore: remove unused local variables",
+          "timestamp": "2025-03-26T07:27:31-06:00",
+          "tree_id": "ba26a2b6e0ce54b406c4d6ccc73b19506060313a",
+          "url": "https://github.com/coinbase/cb-mpc/commit/dc7ce2156cfc61366a0f5f70761dacf22a35008b"
+        },
+        "date": 1742998241748,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "Core/EC/Add/secp256k1",
+            "value": 0.3594652520810311,
+            "unit": "us/iter",
+            "extra": "iterations: 1939555\ncpu: 0.3594433475719946 us\nthreads: 1"
+          },
+          {
+            "name": "Core/EC/Add/Ed25519",
+            "value": 0.24628261159251752,
+            "unit": "us/iter",
+            "extra": "iterations: 2844165\ncpu: 0.2462554127485571 us\nthreads: 1"
+          },
+          {
+            "name": "Core/EC/Multiply/secp256k1",
+            "value": 41.50078333630018,
+            "unit": "us/iter",
+            "extra": "iterations: 16911\ncpu: 41.498773815859494 us\nthreads: 1"
+          },
+          {
+            "name": "Core/EC/Multiply/Ed25519",
+            "value": 50.289644999265356,
+            "unit": "us/iter",
+            "extra": "iterations: 13938\ncpu: 50.286433562921516 us\nthreads: 1"
+          },
+          {
+            "name": "Core/EC/Multiply_G/secp256k1",
+            "value": 19.97770915162169,
+            "unit": "us/iter",
+            "extra": "iterations: 35032\ncpu: 19.977604675725054 us\nthreads: 1"
+          },
+          {
+            "name": "Core/EC/Multiply_G/Ed25519",
+            "value": 7.576458801723659,
+            "unit": "us/iter",
+            "extra": "iterations: 91715\ncpu: 7.575113024041868 us\nthreads: 1"
+          },
+          {
+            "name": "Core/EC/MulAdd/secp256k1",
+            "value": 61.71505729120839,
+            "unit": "us/iter",
+            "extra": "iterations: 11363\ncpu: 61.7036779899674 us\nthreads: 1"
+          },
+          {
+            "name": "Core/EC/MulAdd/Ed25519",
+            "value": 57.54031508988265,
+            "unit": "us/iter",
+            "extra": "iterations: 12187\ncpu: 57.537597357840305 us\nthreads: 1"
           }
         ]
       }
