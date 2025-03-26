2154141c799f17987020a60e9727b3a3732df49f
github-action-benchmark <github@users.noreply.github.com>
2025-03-26 14:10:50 +0000

    add Benchmark (googlecpp) benchmark result for dc7ce2156cfc61366a0f5f70761dacf22a35008b

--- docs/static/ot/data.js
+++ docs/static/ot/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1742577986703,
+  "lastUpdate": 1742998250566,
   "repoUrl": "https://github.com/coinbase/cb-mpc",
   "entries": {
     "Benchmark": [
@@ -42,6 +42,48 @@ window.BENCHMARK_DATA = {
             "value": 13775.621254897524,
             "unit": "us/iter",
             "extra": "iterations: 51\ncpu: 13774.46854901961 us\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "Arash-Afshar@users.noreply.github.com",
+            "name": "Arash Afshar",
+            "username": "Arash-Afshar"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "dc7ce2156cfc61366a0f5f70761dacf22a35008b",
+          "message": "chore: cleanup (#5)\n\n* fix: properly publish commit in benchmark page\n* feat: support go 1.24\n* chore: remove unused local variables",
+          "timestamp": "2025-03-26T07:27:31-06:00",
+          "tree_id": "ba26a2b6e0ce54b406c4d6ccc73b19506060313a",
+          "url": "https://github.com/coinbase/cb-mpc/commit/dc7ce2156cfc61366a0f5f70761dacf22a35008b"
+        },
+        "date": 1742998249764,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "MPC/OT/BaseOT/Step1_R2S/256",
+            "value": 24493.624071429364,
+            "unit": "us/iter",
+            "extra": "iterations: 28\ncpu: 24491.654785714287 us\nthreads: 1"
+          },
+          {
+            "name": "MPC/OT/BaseOT/Step2_S2R/256",
+            "value": 99203.63271430686,
+            "unit": "us/iter",
+            "extra": "iterations: 7\ncpu: 99173.97014285716 us\nthreads: 1"
+          },
+          {
+            "name": "MPC/OT/BaseOT/OutputR/256",
+            "value": 13820.407823530724,
+            "unit": "us/iter",
+            "extra": "iterations: 51\ncpu: 13819.068882352942 us\nthreads: 1"
           }
         ]
       }
